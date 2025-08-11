9d1b0a7a313d5ef888b62de0797b71f4424565b5
Tai Tran <tai.tran.the.dev@gmail.com>
2025-08-11 17:42:54 +0700

    apply  tailwind

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
   runtimeConfig: {
     public: {
       backendUrl: process.env.BACKEND_URL || 'http://localhost:3001'
     }
   },
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui', '@nuxt/fonts'],
   css: ['~/assets/css/tailwind.css'],
-  modules: ['@nuxt/fonts']
 })
