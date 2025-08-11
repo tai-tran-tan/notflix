53ebb2abc351ae8a23ea34548255cac4ecd89189
Tai Tran <tai.tran.the.dev@gmail.com>
2025-08-11 17:22:36 +0700

    install tailwind

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,10 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
   runtimeConfig: {
     public: {
       backendUrl: process.env.BACKEND_URL || 'http://localhost:3001'
     }
   },
-  //css: ['~/assets/css/tailwind.css'], // optional if you use Tailwind
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  css: ['~/assets/css/tailwind.css'],
+  modules: ['@nuxt/fonts']
 })
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './app.vue',
+    './plugins/**/*.{js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
